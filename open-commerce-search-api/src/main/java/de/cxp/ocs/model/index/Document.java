b042c45a58364ab8b6f4d3e408033781cb6d1b2c
Rudolf Batt <rudolf.batt@commerce-experts.com>
2020-01-15 10:52:17 +0100

    Refine Document type and add schema definition preview

--- open-commerce-search-api/src/main/java/de/cxp/ocs/model/index/Document.java
+++ open-commerce-search-api/src/main/java/de/cxp/ocs/model/index/Document.java
@@ -16,27 +16,46 @@ public class Document {
 	@NonNull
 	String id;
 
+	/**
+	 * <pre>
+	 * type: object
+	 * anyOf:
+	 * - type: string
+	 * - type: number
+	 * - type: integer
+	 * - type: boolean
+	 * - $ref: '#/components/schemas/Attribute'
+	 * - type: array
+	 * _ items:
+	 * ___ oneOf:
+	 * _____ - type: string
+	 * _____ - type: number
+	 * _____ - type: integer
+	 * _____ - type: boolean
+	 * _____ - $ref: '#/components/schemas/Attribute'
+	 * </pre>
+	 */
 	Map<String, Object> data = new HashMap<>();
 
 	public Document() {}
 
-	public Document set(String name, String value) {
-		data.put(name, value);
+	public Document set(String name, String... values) {
+		data.put(name, values.length == 1 ? values[0] : values);
 		return this;
 	}
 
-	public Document set(String name, int value) {
-		data.put(name, value);
+	public Document set(String name, int... values) {
+		data.put(name, values.length == 1 ? values[0] : values);
 		return this;
 	}
 
-	public Document set(String name, double value) {
-		data.put(name, value);
+	public Document set(String name, double... values) {
+		data.put(name, values.length == 1 ? values[0] : values);
 		return this;
 	}
 
 	public Document set(String name, Attribute... values) {
-		data.put(name, values);
+		data.put(name, values.length == 1 ? values[0] : values);
 		return this;
 	}
 }
