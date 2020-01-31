e7be91f2911a55e118932be75c69bf7c2aaaf452
Georgi Tonkov <georgi.tonkov@lidl.bg>
2020-01-31 12:30:28 +0200

    PSEAR-1048: api service engine initialization

---
+++ src/main/java/mindshift/mto/connector/ocs/service/OcsSearchService.java
@@ -0,0 +1,24 @@
+package mindshift.mto.connector.ocs.service;
+
+import mindshift.search.connector.models.SearchResult;
+import mindshift.search.connector.models.SearchState;
+
+/**
+ * Open Commerce Search - Search Services.
+ */
+public class OcsSearchService {
+
+    /**
+     * Performs search for a given SearchState.
+     * 
+     * @param request SearchResult
+     * @return SearchResult
+     */
+    public SearchResult search(final SearchState request) {
+        SearchResult searchResult = new SearchResult();
+        searchResult.setId(request.getId());
+        searchResult.setOffset(request.getOffset());
+        searchResult.setQ(request.getQ());
+        return searchResult;
+    }
+}
