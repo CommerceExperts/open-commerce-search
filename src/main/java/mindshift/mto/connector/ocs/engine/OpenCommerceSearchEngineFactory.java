d9239fa93e2690cd4e22712caaee2f641ce6b350
Georgi Tonkov <georgi.tonkov@lidl.bg>
2020-02-10 16:57:44 +0200

    PSEAR-1035: Up Connector API version

--- src/main/java/mindshift/mto/connector/ocs/engine/OpenCommerceSearchEngineFactory.java
+++ src/main/java/mindshift/mto/connector/ocs/engine/OpenCommerceSearchEngineFactory.java
@@ -1,9 +1,9 @@
 package mindshift.mto.connector.ocs.engine;
 
 import java.util.Map;
-import mindshift.mto.connector.api.v2.SearchConnector;
-import mindshift.mto.connector.api.v2.engine.EngineFactory;
 import mindshift.mto.connector.ocs.connector.OpenCommerceSearchConnector;
+import mindshift.search.connector.api.v2.SearchConnector;
+import mindshift.search.connector.api.v2.engine.EngineFactory;
 
 /**
  * Open Commerce Search implementation of the EngineFactory.
