22a4e367b03d01da1aa9a3f5f3f754f452143157
Rudolf Batt <rudolf.batt@commerce-experts.com>
2020-10-20 17:52:14 +0200

    Fix indexing categories properly

--- indexer-service/src/main/java/de/cxp/ocs/indexer/IndexItemConverter.java
+++ indexer-service/src/main/java/de/cxp/ocs/indexer/IndexItemConverter.java
@@ -1,12 +1,16 @@
 package de.cxp.ocs.indexer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import de.cxp.ocs.config.Field;
 import de.cxp.ocs.indexer.model.DataItem;
 import de.cxp.ocs.indexer.model.IndexableItem;
 import de.cxp.ocs.indexer.model.MasterItem;
 import de.cxp.ocs.indexer.model.VariantItem;
+import de.cxp.ocs.model.index.Category;
 import de.cxp.ocs.model.index.Document;
 import de.cxp.ocs.model.index.Product;
 import lombok.NonNull;
@@ -52,21 +56,86 @@ public class IndexItemConverter {
 		boolean isVariant = (targetItem instanceof VariantItem);
 		final Map<String, Object> sourceData = sourceDoc.getData();
 		for (final Field field : fields.values()) {
-			if ((isVariant && field.isVariantLevel() || !isVariant && field.isMasterLevel())) {
-				Object value = sourceData.get(field.getName());
+			if ((field.isVariantLevel() && !isVariant) || (field.isMasterLevel() && isVariant)) {
+				continue;
+			}
 
-				// if there is no value by the field name but there are source
-				// fields defined, check all of them until a value can be
-				// extracted
-				if (value == null && field.getSourceNames() != null && !field.getSourceNames().isEmpty()) {
-					for (int i = 0; value == null && i < field.getSourceNames().size(); i++) {
-						value = sourceData.get(field.getSourceNames().get(i));
+			switch (field.getType()) {
+				case id:
+					// no ID handling here
+					// TODO: use that field information to retrieve an ID from
+					// the data in case the source document itself has not
+					// explicit ID set.
+					break;
+				case category:
+					if (targetItem instanceof IndexableItem) {
+						extractCategoryPaths(field, sourceDoc, targetItem);
 					}
-				}
-				if (value != null) {
-					targetItem.setValue(field, value);
+					break;
+				default:
+					List<String> sourceValues = new ArrayList<>(1);
+					Object value = sourceData.get(field.getName());
+					if (value != null) sourceValues.add(value.toString());
+
+					if (field.getSourceNames() != null) {
+
+						for (int i = 0; i < field.getSourceNames().size(); i++) {
+							Object sourceValue = sourceData.get(field.getSourceNames().get(i));
+							if (sourceValue != null) {
+								sourceValues.add(sourceValue.toString());
+							}
+						}
+						if (sourceValues.size() > 0) {
+							value = sourceValues;
+						}
+					}
+
+					if (!sourceValues.isEmpty()) {
+						targetItem.setValue(field, sourceValues.size() == 1 ? sourceValues.get(0) : sourceValues);
+					}
+			}
+		}
+	}
+
+	private void extractCategoryPaths(final Field field, Document sourceDoc, final DataItem targetItem) {
+		Set<String> categoryPaths = ((IndexableItem) targetItem).getCategories();
+
+		// handle special category field
+		if (sourceDoc.getCategories() != null) {
+			List<Category[]> sourceCategories = sourceDoc.getCategories();
+			for (Category[] categoryPath : sourceCategories) {
+				categoryPaths.add(toCategoryPathString(categoryPath));
+			}
+		}
+		// fallback: try to get categories from the standard data
+		// fields
+		else if (field.getSourceNames() != null && !field.getSourceNames().isEmpty()) {
+			for (int i = 0; i < field.getSourceNames().size(); i++) {
+				Object sourceValue = sourceDoc.getData().get(field.getSourceNames().get(i));
+				if (sourceValue != null) {
+					if (sourceValue instanceof List<?>) {
+						((List<?>) sourceValue).forEach(o -> categoryPaths.add(o.toString()));
+					}
+					else if (sourceValue instanceof String[]) {
+						for (String s : (String[]) sourceValue) {
+							categoryPaths.add(s);
+						}
+					}
+					else {
+						categoryPaths.add(sourceValue.toString());
+					}
 				}
 			}
 		}
+	}
+
+	private String toCategoryPathString(Category[] categories) {
+		StringBuilder categoryPath = new StringBuilder();
+		for (Category c : categories) {
+			if (categoryPath.length() > 0) categoryPath.append('/');
+			categoryPath.append(c.getName().replace("/", "%2F"));
+			if (c.getId() != null) categoryPath.append(':').append(c.getId());
+		}
+		return categoryPath.toString();
 	}
 }
