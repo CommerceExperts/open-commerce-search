584d4c4daf9e0512f35af3a39b602a9bff265b7e
Rudolf Batt <rudolf.batt@commerce-experts.com>
2023-01-26 14:24:02 +0100

    Fix errors because of unescaped characters

--- search-service/src/main/java/de/cxp/ocs/elasticsearch/query/builder/NgramQueryFactory.java
+++ search-service/src/main/java/de/cxp/ocs/elasticsearch/query/builder/NgramQueryFactory.java
@@ -5,13 +5,12 @@ import static de.cxp.ocs.config.QueryBuildingSetting.minShouldMatch;
 import static de.cxp.ocs.config.QueryBuildingSetting.multimatch_type;
 import static de.cxp.ocs.config.QueryBuildingSetting.tieBreaker;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder.Type;
@@ -21,6 +20,7 @@ import de.cxp.ocs.config.FieldConfigAccess;
 import de.cxp.ocs.config.FieldConstants;
 import de.cxp.ocs.config.QueryBuildingSetting;
 import de.cxp.ocs.elasticsearch.query.MasterVariantQuery;
+import de.cxp.ocs.elasticsearch.query.model.EscapeUtil;
 import de.cxp.ocs.elasticsearch.query.model.QueryStringTerm;
 import de.cxp.ocs.spi.search.ESQueryFactory;
 import de.cxp.ocs.util.ESQueryUtils;
@@ -82,25 +82,24 @@ public class NgramQueryFactory implements ESQueryFactory {
 
 	@Override
 	public MasterVariantQuery createQuery(List<QueryStringTerm> searchTerms) {
-		StringBuilder searchPhrase = new StringBuilder();
 		// TODO: do a ngram tokenization on each term separately and search each
 		// "ngramed word" separately combined with boolean-should-clauses but
 		// each using "best-field" match strategy.
 		// minShouldMatch setting is then used twice: per splitted field and for
 		// the words itself (the should clauses)
-		searchTerms.forEach(term -> searchPhrase.append(term.getWord()).append(" "));
+		String searchPhrase = searchTerms.stream()
+				.map(QueryStringTerm::getWord)
+				.map(EscapeUtil::escapeReservedESCharacters)
+				.collect(Collectors.joining(" "));
 
-		// TODO: use locale for lowercase or better: use elasticsearch analyzer!
-		String ngramPhrase = buildNgrams(searchPhrase.toString().trim().toLowerCase());
-
-		MultiMatchQueryBuilder mainQuery = buildEsQuery(ngramPhrase);
+		MultiMatchQueryBuilder mainQuery = buildEsQuery(searchPhrase);
 		if (masterFields.size() > 0) {
 			mainQuery.fields(masterFields);
 		}
 		String queryName = getLabel(searchTerms);
 		mainQuery.queryName(queryName);
 
-		MultiMatchQueryBuilder variantQuery = buildEsQuery(ngramPhrase);
+		MultiMatchQueryBuilder variantQuery = buildEsQuery(searchPhrase);
 		if (variantFields.size() > 0) {
 			variantQuery.fields(variantFields);
 		}
@@ -121,7 +120,7 @@ public class NgramQueryFactory implements ESQueryFactory {
 	private MultiMatchQueryBuilder buildEsQuery(String ngramPhrase) {
 		MultiMatchQueryBuilder mainQuery = QueryBuilders
 				.multiMatchQuery(ngramPhrase)
-				.analyzer("whitespace")
+				.analyzer("ngram")
 				.fuzziness(Fuzziness.ZERO)
 				.minimumShouldMatch(getMinShouldMatch())
 				.tieBreaker(Util.tryToParseAsNumber(querySettings.getOrDefault(tieBreaker, "0")).orElse(0).floatValue())
@@ -132,35 +131,6 @@ public class NgramQueryFactory implements ESQueryFactory {
 
 	private String getMinShouldMatch() {
 		return querySettings.getOrDefault(minShouldMatch, "70%");
-	}
-
-	protected String buildNgrams(String searchPhrase) {
-		if (searchPhrase == null || searchPhrase.length() < 2) return searchPhrase;
-
-		List<String> ngrams = new ArrayList<>();
-		ngrams.add("_" + searchPhrase.substring(0, 2));
-
-		// in: damenkleider
-		// out: _da dam ame men enk nkl kle lei eid ide der er_
-		for (int i = 0; i < searchPhrase.length() - 2; i++) {
-			boolean foundWhiteSpace = false;
-			if (searchPhrase.charAt(i) == ' ') {
-				if (i + 3 <= searchPhrase.length()) {
-					ngrams.add("_" + searchPhrase.substring(i + 1, i + 3));
-				}
-				foundWhiteSpace = true;
-			}
-			if (searchPhrase.charAt(i + 2) == ' ') {
-				ngrams.add(searchPhrase.substring(i, i + 2) + "_");
-				i++;
-				foundWhiteSpace = true;
-			}
-			if (!foundWhiteSpace) {
-				ngrams.add(searchPhrase.substring(i, i + 3));
-			}
-		}
-		ngrams.add(searchPhrase.substring(searchPhrase.length() - 2) + "_");
-		return StringUtils.join(ngrams, " ");
 	}
 
 	@Override
