0cb502cab9302d1584c655c309f911f10de091dc
Rudolf Batt <rudolf.batt@commerce-experts.com>
2023-07-14 17:38:44 +0200

    Fix java-doc of LinkBuilder

--- ocs-plugin-spi/src/main/java/de/cxp/ocs/util/LinkBuilder.java
+++ ocs-plugin-spi/src/main/java/de/cxp/ocs/util/LinkBuilder.java
@@ -1,7 +1,9 @@
 package de.cxp.ocs.util;
 
 /**
- * Interface for internal utility function exposed to the provided plugins.
+ * Interface for internal utility function exposed to the provided plugins. The link builder is immutable and
+ * <strong>always</strong> uses the 'current URL state' as a basis. So each call just returns a copy state reflected as
+ * an URL and does not change the linkbuilder itself.
  */
 public interface LinkBuilder {
 
@@ -9,9 +11,13 @@ public interface LinkBuilder {
 	 * Returns an URL with the specified filter(s) set. If that filter already exists, the values will be merged in case
 	 * the mergeValues option is set in the configuration. Otherwise the values are replaced.
 	 * 
-	 * @param filterConfig
+	 * @param filtername
+	 *        name of the filter to add
+	 * @param mergeValues
+	 *        set true to merge values in case of existing filter, to replace existing values set to false
 	 * @param values
-	 * @return
+	 *        the values the filter should have
+	 * @return the created URL
 	 */
 	String withFilterAsLink(String filtername, boolean mergeValues, String... values);
 
@@ -22,9 +28,11 @@ public interface LinkBuilder {
 	 * No multi-select value merging will be done.
 	 * </p>
 	 * 
-	 * @param facetConfig
-	 * @param filterInputValues
-	 * @return
+	 * @param filtername
+	 *        name of the filter to add
+	 * @param values
+	 *        the values the filter should have
+	 * @return the created filter URL
 	 */
 	default String withExactFilterAsLink(String filtername, String... values) {
 		return withFilterAsLink(filtername, false, values);
@@ -33,7 +41,8 @@ public interface LinkBuilder {
 	/**
 	 * Returns an URL without the filter parameter of that according facet.
 	 * 
-	 * @param facetConfig
+	 * @param filtername
+	 *        the filter to remove from the current ulr.
 	 * @return
 	 */
 	String withoutFilterAsLink(String filtername);
