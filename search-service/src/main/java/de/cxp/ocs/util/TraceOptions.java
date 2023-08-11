830d03275911accd4c53ee65539af21c0470ec25
Rudolf Batt <rudolf.batt@commerce-experts.com>
2023-08-11 11:41:35 +0200

    Improve TraceOptions implementation

--- search-service/src/main/java/de/cxp/ocs/util/TraceOptions.java
+++ search-service/src/main/java/de/cxp/ocs/util/TraceOptions.java
@@ -1,13 +1,13 @@
 package de.cxp.ocs.util;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import org.apache.commons.lang3.StringUtils;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 public class TraceOptions {
 
@@ -15,16 +15,30 @@ public class TraceOptions {
 		Request, EsQuery
 	}
 
+	private static Map<String, TraceFlag> lowercaseMapping = new HashMap<>(TraceFlag.values().length);
+	static {
+		for (TraceFlag f : TraceFlag.values()) {
+			lowercaseMapping.put(f.name().toLowerCase(), f);
+		}
+	}
+
 	public static final TraceOptions OFF = new TraceOptions(Collections.emptySet());
-	
+
 	private final Set<TraceFlag> traceFlags;
-	
+
 	public static TraceOptions parse(String paramValue) {
+		if (paramValue == null || paramValue.isBlank()) return OFF;
+
 		String[] split = StringUtils.split(paramValue, ',');
-		Set<TraceFlag> traceFlags = new HashSet<>(TraceFlag.values().length);
+		Set<TraceFlag> traceFlags = EnumSet.noneOf(TraceFlag.class);
 		for (String s : split) {
-			TraceFlag flag = TraceFlag.valueOf(s);
-			traceFlags.add(flag);
+			TraceFlag traceFlag = lowercaseMapping.get(s.trim().toLowerCase());
+			if (traceFlag != null) {
+				traceFlags.add(traceFlag);
+			}
+			else {
+				log.debug("ignoring unknown trace-flag " + s);
+			}
 		}
 		return new TraceOptions(traceFlags);
 	}
