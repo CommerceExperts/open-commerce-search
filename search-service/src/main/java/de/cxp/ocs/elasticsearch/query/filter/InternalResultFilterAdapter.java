54291d62ca08737f1bc06255c67f8404c016077d
Rudolf Batt <rudolf.batt@commerce-experts.com>
2023-09-06 17:51:10 +0200

    Fix missing field-prefix separator for FILTER fields

--- search-service/src/main/java/de/cxp/ocs/elasticsearch/query/filter/InternalResultFilterAdapter.java
+++ search-service/src/main/java/de/cxp/ocs/elasticsearch/query/filter/InternalResultFilterAdapter.java
@@ -6,6 +6,16 @@ import de.cxp.ocs.elasticsearch.model.filter.InternalResultFilter;
 
 public interface InternalResultFilterAdapter<F extends InternalResultFilter> {
 
+	/**
+	 * Build query for given filter using the specified field-prefix.
+	 * Negation of the filter MUST NOT be handled inside the adapter!
+	 * 
+	 * @param fieldPrefix
+	 *        the prefix to the field name including the separator "."
+	 * @param filter
+	 *        the filter with name and values and all required information.
+	 * @return
+	 */
 	QueryBuilder getAsQuery(String fieldPrefix, F filter);
 
 }
