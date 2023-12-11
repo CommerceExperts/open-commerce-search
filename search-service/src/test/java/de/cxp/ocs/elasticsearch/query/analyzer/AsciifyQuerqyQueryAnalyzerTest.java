b6a27a925395a3e57596fac5b42b3e67a66a2f0d
Rudolf Batt <rudolf.batt@commerce-experts.com>
2023-12-11 12:08:26 +0100

    Adjust querqy analyzer for changed filter quoting (005258a5)

--- search-service/src/test/java/de/cxp/ocs/elasticsearch/query/analyzer/AsciifyQuerqyQueryAnalyzerTest.java
+++ search-service/src/test/java/de/cxp/ocs/elasticsearch/query/analyzer/AsciifyQuerqyQueryAnalyzerTest.java
@@ -155,13 +155,13 @@ public class AsciifyQuerqyQueryAnalyzerTest {
 	@Test
 	public void testDiacriticCharTermMatchesAsciifiedFilterRule() throws InterruptedException, ExecutionException {
 		var analyzedQuery = analyze("bùty");
-		assertEquals("(bùty OR booty) OR (buty) -obuwie", analyzedQuery.toQueryString());
+		assertEquals("(bùty OR booty) OR (buty) -\"obuwie\"", analyzedQuery.toQueryString());
 	}
 
 	@Test
 	public void testMultipleTermsMatchesAllKindOfRule() throws InterruptedException, ExecutionException {
 		var analyzedQuery = analyze("dziecięce zimowe bùty");
-		assertEquals("((dziecięce OR dziewczęce^0.5) (zimowe OR śnieg) (bùty OR booty)) OR ((dzieciece OR chłopięce^0.5 OR dzieci) (zimowe OR śnieg) buty) -obuwie", analyzedQuery.toQueryString());
+		assertEquals("((dziecięce OR dziewczęce^0.5) (zimowe OR śnieg) (bùty OR booty)) OR ((dzieciece OR chłopięce^0.5 OR dzieci) (zimowe OR śnieg) buty) -\"obuwie\"", analyzedQuery.toQueryString());
 	}
 
 	@Test
