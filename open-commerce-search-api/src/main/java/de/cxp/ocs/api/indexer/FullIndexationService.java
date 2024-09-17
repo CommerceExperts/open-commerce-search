1c140a170e6c3b370b3ca38cb2f747bc7097bbaf
Rudolf Batt <rudolf.batt@commerce-experts.com>
2024-09-17 14:53:39 +0200

    Switch from javax to jakarta

--- open-commerce-search-api/src/main/java/de/cxp/ocs/api/indexer/FullIndexationService.java
+++ open-commerce-search-api/src/main/java/de/cxp/ocs/api/indexer/FullIndexationService.java
@@ -1,7 +1,5 @@
 package de.cxp.ocs.api.indexer;
 
-import javax.ws.rs.*;
-
 import de.cxp.ocs.model.index.BulkImportData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -15,6 +13,9 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
 
 /**
  * Run a full import into a new index.
