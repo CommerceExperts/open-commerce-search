78a48f75cfe5567fb47c7a2a9113fb72fd6d8327
nelsonfrz <nelson.frz@proton.me>
2025-07-04 17:30:23 +0200

    add temp cors config to config-management-service

---
+++ config-management-service/src/main/java/de/cxp/ocs/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package de.cxp.ocs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
